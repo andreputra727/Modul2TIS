31f3c07dce0ca830def5293f64451372b25fd6ac
andreputra727 <andreputrasrg946@gmail.com>
2026-03-06 22:33:07 +0700

    Initialize Express app with MongoDB connection

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const bookRoutes = require('./routes/book.route');
+
+mongoose.connect(process.env.MONGO_URI);
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.log(error);
+});
+
+db.once('connected', () => {
+  console.log('Mongo connected');
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Andre Syaputra Siregar, 245150707111045'  
+  });
+});
+
+app.use('/books', bookRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
