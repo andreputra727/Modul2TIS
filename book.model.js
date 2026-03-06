1f0f03657ea4b42851babcd048d9c173e07a0061
andreputra727 <andreputrasrg946@gmail.com>
2026-03-06 22:34:19 +0700

    Create book model with schema definition

---
+++ book.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String },
+  author: { type: String },
+  year: { type: Number },
+  pages: { type: Number },
+  summary: { type: String },
+  publisher: { type: String }
+});
+
+module.exports = mongoose.model('book', bookSchema);
